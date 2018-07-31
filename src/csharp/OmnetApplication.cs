41e004a90e3971f5af9688004ad68b422e875f74
Jens Drenhaus <jensdrenhaus@gmail.com>
2018-07-31 17:40:38 +0200

    update gitignore, cleanup code

--- src/csharp/OmnetApplication.cs
+++ src/csharp/OmnetApplication.cs
@@ -14,13 +14,12 @@ namespace OmnetServices
 	// You can use the OmnetSimulation class to
 
 	// Example:
-    // This example creates two nodes. When the simulation starts node1 sends a message to node2
-	// an the global timer is set to 2 seconds. when the timer has expired, node1 sends again 
-	// and the timer is again set to 2 seconds .. never ending story ;)
-	// For the incoming events, the current simulation time is output.
+    // ToDo
 
     public class OmnetInterface
     {
+	    public static readonly OmnetInterface Instance = new OmnetInterface();
+		public static ActorSystem Sys { get; set; }
 	    public ulong TimeAtStart { get; set; }
 	    public DateTime DateTimeOffsetAtStart { get; set; }
 	    
@@ -57,14 +56,12 @@ namespace OmnetServices
 		    Sys = ActorSystem.Create("sys", configParsed);
 	    }
 	    
-	    protected virtual void OnOmneTBeforeStart()
-	    {
-		    OMNeTBeforeStart?.Invoke();
-	    }
-
+	    // protected virtual void OnOmneTBeforeStart()
+	    // {
+		//     OMNeTBeforeStart?.Invoke();
+	    // }
 	    public delegate void OnBeforeOMNeTStart();
 	    public event OnBeforeOMNeTStart OMNeTBeforeStart;
-	    
         public static void initSimulation()
         {
             Console.WriteLine("C# : initSimulation is called");
@@ -72,10 +69,6 @@ namespace OmnetServices
 	        Instance.TimeAtStart = OmnetSimulation.Instance().GetGlobalTime();
 	        Instance.DateTimeOffsetAtStart = DateTime.Now;
 	        
-            //Example
-            // OmnetSimulation.Instance().CreateNode(1);
-            // OmnetSimulation.Instance().CreateNode(2);
-            // OmnetSimulation.Instance().GetGlobalTime();
 	        // Context = new BulkExecutingSynchronizationContext();
 	        // SynchronizationContext.SetSynchronizationContext(Context);
 	        
@@ -98,15 +91,11 @@ namespace OmnetServices
 	        Console.WriteLine("Told Container to configure.");
 
 	        // Actually trigger the event.
-	        Instance.OnOmneTBeforeStart();
+	        Instance.OMNeTBeforeStart?.Invoke();
 	        // TODO Context.ExecuteAll();
 	        Console.WriteLine("Done with init.");
 	        // Here everything is ready and we can wait for omnet++.
         }
-
-	    public static ActorSystem Sys { get; set; }
-	    public static readonly OmnetInterface Instance = new OmnetInterface();
-
 
 		public static void timerNotify(ulong nodeId)
         {
@@ -115,6 +104,8 @@ namespace OmnetServices
             //...
         }
 
+		public delegate void OnOmnetGlobalTimerNotify();
+	    public event OnOmnetGlobalTimerNotify OmnetGlobalTimerNotify;
 		public static void globalTimerNotify()
         {
             Console.WriteLine("C# : globalTimerNotify is called");
@@ -125,44 +116,36 @@ namespace OmnetServices
             // OmnetSimulation.Instance().SetGlobalTimerSeconds(2);
 	        Instance.OmnetGlobalTimerNotify?.Invoke();
         }
-
-	    public delegate void OnOmnetGlobalTimerNotify();
-
-	    public event OnOmnetGlobalTimerNotify OmnetGlobalTimerNotify;
 	    
 	    #region Start
-	    protected virtual void OnOmneTStart()
-	    {
-		    OMNeTStart?.Invoke();
-	    }
-
+	    // protected virtual void OnOmneTStart()
+	    // {
+		//     OMNeTStart?.Invoke();
+	    // }
 	    public delegate void OnOMNeTStart();
-
 	    public event OnOMNeTStart OMNeTStart;
-
 	    // method invoked by omnet++ at simulation start
 	    public static void simulationReady()
 	    {
 		    Console.WriteLine("Received simulation ready from omnet++.");
-		    Instance.OnOmneTStart();
+		    Instance.OMNeTStart?.Invoke();
 		    // TODO Context.ExecuteAll();
 	    }
 	    #endregion
 
 	    #region Receiving
-	    protected virtual void OnOmneTReceive(ulong dest, ulong src, int id, int statusFlag)
-	    {
-		    OMNeTReceive?.Invoke(dest, src, id, statusFlag);
-	    }
-
+	    // protected virtual void OnOmneTReceive(ulong dest, ulong src, int id, int statusFlag)
+	    // {
+		//     OMNeTReceive?.Invoke(dest, src, id, statusFlag);
+	    // }
 	    public delegate void OnOMNeTReceive(ulong dest, ulong source, int id, int statusFlag);
-
 	    public event OnOMNeTReceive OMNeTReceive;
+
 	    // method invoked by omnet++ at reception events
 	    public static void receptionNotify(ulong dest, ulong source, int id, int statusFlag)
 	    {
 		    Console.WriteLine("Received reception notify from omnet++.");
-		    Instance.OnOmneTReceive(dest, source, id, statusFlag);
+		    Instance.OMNeTReceive?.Invoke(dest, source, id, statusFlag);//Instance.OnOmneTReceive(dest, source, id, statusFlag);
 		    // TODO Context.ExecuteAll();
 	    }
 	    #endregion
